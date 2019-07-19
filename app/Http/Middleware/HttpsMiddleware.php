c6739ae99a084f288e88c63fe894e7ce26ee9ce0
Charrière Loïc <loic.charriere@students.bfh.ch>
2019-07-19 10:03:52 +0200

    https middleware

---
+++ app/Http/Middleware/HttpsMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class HttpsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!$request->secure() && App::environment() === 'production') {
+            return redirect()->secure($request->getRequestUri());
+        }
+    }
+}
